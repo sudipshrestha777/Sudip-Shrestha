9521c38d0dfcb302796f55be4319eaa4e9dbe7f3
sudipshrestha777 <144550844+sudipshrestha777@users.noreply.github.com>
2024-11-23 15:05:30 +0545

    Delete lab03_02.js
    delete message

--- lab03_02.js
+++
@@ -1,6 +0,0 @@
-
-var username = localStorage.getItem("username");
-var password = localStorage.getItem("password");
-
-var displayContainer = document.getElementById("displayContainer");
-displayContainer.innerText = "Username: " + username + "\nPassword: " + password;
