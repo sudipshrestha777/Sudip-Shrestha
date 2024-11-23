73f4da01c7f51add20d1d5e7bd1cb630b549f6c1
sudipshrestha777 <144550844+sudipshrestha777@users.noreply.github.com>
2024-11-23 15:04:43 +0545

    Delete lab03.js
    Delete message

--- lab03.js
+++
@@ -1,9 +0,0 @@
-function btn_actn() {
-    var username = document.getElementById("id").value;
-    var password = document.getElementById("pw").value;
-
-    localStorage.setItem("username", username);
-    localStorage.setItem("password", password);
-
-    window.location.href = "lab3_2.html";
-}
